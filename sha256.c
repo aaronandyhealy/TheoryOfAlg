e52288c9d9ab139ae3bc5fe0ce5d07d314028b5d
aaronandyhealy <aaronhealy2010@hotmail.com>
2019-03-08 10:12:00 +0000

    main & sha256 method added

---
+++ sha256.c
@@ -0,0 +1,19 @@
+// Aaron Healy 2019
+//Secure Hash Algorithim, 256 bit version
+
+//The usual input/output header file
+#include <stdio.h>
+//for using fixed bit length ints
+#include <stdint.h>
+
+void sha256();
+
+int main(int argc, char *argv[]){
+    sha256();
+    
+    return 0;
+}
+
+void sha256{
+
+}
