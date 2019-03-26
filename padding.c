eb1513a84ffdb5dc782ecafde6f1b95ba7cba8fc
aaronandyhealy <aaronhealy2010@hotmail.com>
2019-03-26 10:53:16 +0000

    Bugs in Sha256 fixed

--- padding.c
+++ padding.c
@@ -12,17 +12,35 @@ union msgblock {
 int main(int argc, char *argv[]){
     union msgblock M;
 
+    uint64_t nobits = 0;
     uint64_t nobytes;
 
-    FILE* f;
-    f = fopen (argv[1], "r");
+    FILE* file;
+    file = fopen (argv[1], "r");
 
     //loop until file end
-    while(!feof (f)){
-        nobytes = fread(M,.e, 1, 64, f);
-        printf("%llu\n", nobytes);
-    }
-    fclose(f);
+    while(!feof (file)){
 
+        nobytes = fread(M.e, 1, 64, file);
+        printf("Read %2llu bytes \n", nobytes);
+        nobits = nobits + (nobytes * 8);
+
+        if (nobytes < 56) {
+            printf("I've found a block with less than 55 bytes \n");
+            M.e[nobytes] = 0x80;
+            while(nobytes < 56){
+                nobytes = nobytes + 1;
+                M.e[nobytes] = 0x00;
+            }
+            M.s[7] = nobits;
+        }
+    }
+
+    fclose(file);
+
+    for(int i = 0; i < 64; i++){
+        printf("%x ",M.e[i]);
+    }
+    printf("\n");
     return 0;
 }
